0420900e474bf7742ab5d2ab13701ac395aaa4c7
mamasidikova <sevara.mamasidikova@mail.ru>
2022-09-29 22:47:56 +0300

    Docker-compose added

--- backend/foodgram/recipes/management/commands/load_ingredients.py
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -1,21 +1,20 @@
-import csv
-import os
+import json
 
 from django.core.management.base import BaseCommand
-from foodgram.settings import BASE_DIR
-from recipes.models import Ingredient
 
-NEW_BASE_DIR = os.path.split(os.path.split(BASE_DIR)[0])[0]
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
     help = 'Import ingredients from json'
 
-    def handle(self, *args, **options):
-        with open(NEW_BASE_DIR + r"/data/ingredients.csv", 'r',
-                  encoding='utf-8') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                print(row)
-                name, unit = row
-                Ingredient.objects.get_or_create(name=name, unit=unit)
+    def add_arguments(self, parser):
+        parser.add_argument('path', nargs='+', type=str)
+
+    def handle(self, path: str, **options):
+        with open(path[0]) as file:
+            for item in json.load(file):
+                Ingredient.objects.get_or_create(
+                    name=item['name'],
+                    measurement_unit=item['measurement_unit']
+                )
